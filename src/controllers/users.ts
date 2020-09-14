b4166d9f84f6c91969f310c866f4b8fde6acbcce
Leonardo <leeo.apc@gmail.com>
2020-09-14 05:52:17 -0300

    decode token module

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Post } from '@overnightjs/core';
+import { Controller, Get, Middleware, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import { User } from '@src/models/user';
 import { BaseController } from '.';
 import AuthService from '@src/services/auth';
 import ApiError from '@src/util/errors/api-error';
+import { AuthMiddleware } from '@src/middlewares/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -38,5 +39,21 @@ export class UsersController extends BaseController {
     }
     const token = AuthService.generateToken(user.toJSON());
     return res.status(200).send({ token });
+  }
+
+  @Get('me')
+  @Middleware(AuthMiddleware)
+  public async me(req: Request, res: Response): Promise<Response> {
+    const email = req.decoded ? req.decoded.email : undefined;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return this.sendErrorResponse(res, {
+        code: 404,
+        message: 'User not found!',
+      });
+    }
+
+    return res.send({ user });
   }
 }
